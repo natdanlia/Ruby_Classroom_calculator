33786e0126900d7187a10939e08d008028cca510
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-08-22 23:54:13 -0400

    getting averge score was difficult for me

---
+++ starter_files/classroom.rb
@@ -0,0 +1,75 @@
+# Given a grade_hash, student name, and assignment number, return the score
+# for that student and assignment. Note that Ruby counts arrays from 0, but
+# we are referring to them as 1-10.
+def assignment_score(grade_hash, student, assignment_num)
+  grade_hash[student][assignment_num - 1]
+end
+
+# Given a grade_hash and assignment number, return all scores for that
+# assignment. Note that Ruby counts arrays from 0, but we are referring to
+# them as 1-10.
+def assignment_scores(grade_hash, assignment_num)
+    grade_hash.map do |key, value|
+    value[assignment_num - 1]
+  end
+end
+
+# Given a grade_hash and assignment number, return the average score for that
+# assignment. Note that Ruby counts arrays from 0, but we are referring to
+# them as 1-10.
+def assignment_average_score(grade_hash, assignment_num)
+  @assg_score = grade_hash.map do |key, value|
+      value[assignment_num - 1]
+  end
+  @sum_score =@assg_score.reduce do |sum, x|
+             sum + x
+  end
+  def avg_score
+    @sum_score/@assg_score.size
+  end
+    avg_score
+end
+
+# Return a hash of students and their average score.
+# TIP: To convert an array like [[:indiana, 90], [:nevada, 80]] to a hash,
+# use .to_h. Also look at Hash#transform_values.
+def averages(grade_hash)
+  # $add = grade_hash reduce do |sum, x|
+  #   sum + x
+  # end
+  # def avg_res
+  #   $add/
+end
+
+# Return a letter grade for a numerical score.
+# 90+ => A
+# 80-89 => B
+# 70-79 => C
+# 60-69 => D
+# < 60 => F
+def letter_grade(score)
+  if score >=90
+    "A"
+  elsif score < 90 && score >= 80
+    "B"
+  elsif score < 80 && score >=70
+    "C"
+  elsif score < 70 && score >=60
+    "D"
+  else
+    "F"
+  end
+end
+
+# Return a hash of students and their final letter grade, as determined
+# by their average.
+def final_letter_grades(grade_hash)
+end
+
+# Return the average for the entire class.
+def class_average(grade_hash)
+end
+
+# Return an array of the top `number_of_students` students.
+def top_students(grade_hash, number_of_students)
+end
